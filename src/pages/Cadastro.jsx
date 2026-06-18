1a26b83d5a9e8cb17c2fe9c68b75e5be3c0e2479
ApenasGui <guilherme.s.costa1@gmail.com>
2026-06-18 15:46:17 -0300

    adiciona cadastro dos jogos

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext";
 import {addJogo} from "../services/api-jogos";
 import { useItem } from "../contexts/ItemContext";
 
